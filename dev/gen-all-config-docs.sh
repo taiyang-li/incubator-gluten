f9d0f3984b981d02833d8b821a4cf6376a9269ce
taiyang-li <taiyangli@apache.org>
2026-07-15 15:33:46 +0800

    wip

--- dev/gen-all-config-docs.sh
+++ dev/gen-all-config-docs.sh
@@ -21,13 +21,13 @@ BASEDIR=$(dirname "$0")
 MVN_CMD="${BASEDIR}/../build/mvn"
 
 GLUTEN_UPDATE="${GLUTEN_UPDATE:-1}" \
-#${MVN_CMD} clean test \
-#  -Pspark-4.0 -Pscala-2.13 -Pbackends-velox -pl backends-velox -am \
-#  -Dtest=none \
-#  -DfailIfNoTests=false \
-#  -DwildcardSuites=org.apache.gluten.config.AllGlutenConfiguration,org.apache.gluten.config.AllVeloxConfiguration
+${MVN_CMD} clean test \
+  -Pspark-4.0 -Pscala-2.13 -Pbackends-velox -pl backends-velox -am \
+  -Dtest=none \
+  -DfailIfNoTests=false \
+  -DwildcardSuites=org.apache.gluten.config.AllGlutenConfiguration,org.apache.gluten.config.AllVeloxConfiguration
 
-${MVN_CMD} test \
+${MVN_CMD} clean test \
   -Pspark-3.5 -Pscala-2.12 -Pbackends-bolt -pl backends-bolt -am \
   -Dtest=none \
   -DfailIfNoTests=false \
